21e85be66995c81c2a8426d0b87f9164264ae0a0
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2025-03-12 23:19:21 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from this origin
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST'],
   credentials: true
 }));
--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ChatArea = () => {
   return (
     <div className='chatArea-container'>
-        <div className="chatarea-header">ChatArea-Header</div>
+        <div className="chatArea-header">ChatArea-Header</div>
         <div className="message-container">message-container</div>
         <div className="text-input-area">text input area </div>
     </div>
