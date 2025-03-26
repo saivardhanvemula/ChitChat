dd48697485c5dcd792485a4937254bbd230b4c86
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2025-03-27 02:10:45 +0530

    Added Schemas

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,20 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express=require('express')
+const dotenv=require("dotenv")
+// const cors=requrire('cors')
+// const { MongoClient } = require('mongodb')
 
-const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+const app=express()
+dotenv.config();
+// app.use(cors())
+app.use(express.json())
+const port =process.env.port
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
-});
+// const uri = 'mongodb://localhost:27017'
+// const client = new MongoClient(uri)
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+app.get("/",(req,res)=>{
+    res.send("This is backend server")
+})
 
-  socket.on('message', (msg) => {
-    io.emit('message', msg); // Broadcast message to all clients
-  });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+app.listen(port,()=>{console.log(`The server running on http://localhost:${port}/`)})
---
+++ backend/test.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg); // Broadcast message to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
